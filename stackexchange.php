f5f99602d164186db98f7a1dbb4af163e7118f96
Andreas Sander <andreas.sander@hotmail.de>
2012-12-01 01:08:34 +0100

    Initial stackexchange class commit!

---
+++ stackexchange.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * Author: Andreas Sander
+ * Creation: 01.12.12
+ */
+class stackexchange
+{
+    /*
+     * Constructor
+     * @param string: Your StackExchange API Token
+     */
+    function __construct($_token) {
+
+    }
+}
